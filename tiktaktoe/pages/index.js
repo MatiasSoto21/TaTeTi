55c69384b09fc80e7eb1892b5044fcfd0014e828
MatiasSoto21 <matiassoto00@outlook.com>
2023-04-07 00:42:36 -0300

    tateti init settings components

---
+++ tiktaktoe/pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Game from "@/components/Game";
+import Board from "@/components/Board";
+
+
+export default function Home() {
+
+ const handleClick = () => {
+
+ }
+
+ const jumpTo = () => {
+
+ }
+
+ const renderMoves = () => {
+
+ }
+
+  return (
+    <div>TIKTAKTOE
+      <Board onClick={handleClick} />
+    </div>
+  )
+}
